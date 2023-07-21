ec98c79cc7addf266034a0c2f54e3664e1636efa
henrikpoa <henrik_poa@hotmail.com>
2023-07-21 12:26:53 -0300

    get clients page

--- src/main/java/com/henrik/desafio3/controllers/ClientController.java
+++ src/main/java/com/henrik/desafio3/controllers/ClientController.java
@@ -1,6 +1,8 @@
 package com.henrik.desafio3.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,5 +23,9 @@ public class ClientController {
 		ClientDTO dto = service.findById(id);
 		return dto;
 	}
-
+	
+	@GetMapping
+	public Page <ClientDTO> findAll (Pageable pageable) {		
+		return service.findAll(pageable);
+	}
 }
--- src/main/java/com/henrik/desafio3/services/ClientService.java
+++ src/main/java/com/henrik/desafio3/services/ClientService.java
@@ -3,6 +3,8 @@ package com.henrik.desafio3.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -24,4 +26,9 @@ public class ClientService {
 		return dto;
 	}
 	
+	@Transactional(readOnly = true)
+	public Page <ClientDTO> findAll (Pageable pageable) {
+		Page<Client> result = repository.findAll(pageable);						
+		return result.map(x -> new ClientDTO(x));
+	}
 }
