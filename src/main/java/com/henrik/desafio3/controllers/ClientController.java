0d96f0764902eb6ecfdc3fcfcedc262de0d50200
henrikpoa <henrik_poa@hotmail.com>
2023-07-21 10:36:20 -0300

    get client by id

--- src/main/java/com/henrik/desafio3/controllers/ClientController.java
+++ src/main/java/com/henrik/desafio3/controllers/ClientController.java
@@ -1,26 +1,25 @@
 package com.henrik.desafio3.controllers;
 
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.henrik.desafio3.entities.Client;
-import com.henrik.desafio3.repositories.ClientRepository;
+import com.henrik.desafio3.dto.ClientDTO;
+import com.henrik.desafio3.services.ClientService;
 
 @RestController
 @RequestMapping(value = "/clients")
 public class ClientController {
 	
 	@Autowired
-	private ClientRepository repository;
+	private ClientService service;
 	
-	@GetMapping
-	public String test() {
-		Optional<Client> result = repository.findById(1L);
-		return result.get().getName();
+	@GetMapping(value = "/{id}")
+	public ClientDTO findById (@PathVariable Long id) {
+		ClientDTO dto = service.findById(id);
+		return dto;
 	}
 
 }
--- src/main/java/com/henrik/desafio3/services/ClientService.java
+++ src/main/java/com/henrik/desafio3/services/ClientService.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.henrik.desafio3.dto.ClientDTO;
 import com.henrik.desafio3.entities.Client;
@@ -15,6 +16,7 @@ public class ClientService {
 	@Autowired
 	private ClientRepository repository;
 	
+	@Transactional(readOnly = true)
 	public ClientDTO findById (Long id) {
 		Optional<Client> result = repository.findById(id);
 		Client client = result.get();
